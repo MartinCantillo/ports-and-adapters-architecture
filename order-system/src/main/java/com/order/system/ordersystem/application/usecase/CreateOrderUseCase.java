ef08d3a865f90281bc6e0c5d5f024189481e04be
Martin9 <martinjcantillo9@gmail.com>
2026-05-24 18:53:17 -0500

    feat: CreateOrderUseCase was added

--- order-system/src/main/java/com/order/system/ordersystem/application/usecase/CreateOrderUseCase.java
+++ order-system/src/main/java/com/order/system/ordersystem/application/usecase/CreateOrderUseCase.java
@@ -1,4 +1,22 @@
 package com.order.system.ordersystem.application.usecase;
 
+import com.order.system.ordersystem.domain.model.Order;
+import com.order.system.ordersystem.domain.ports.OrderRepositoryPort;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CreateOrderUseCase {
+
+    private final OrderRepositoryPort repositoryPort;
+
+    public CreateOrderUseCase(OrderRepositoryPort repositoryPort) {
+        this.repositoryPort = repositoryPort;
+    }
+
+    public Order execute(String customerName, Double total) {
+
+        Order order = new Order(customerName, total);
+
+        return this.repositoryPort.save(order);
+    }
 }
